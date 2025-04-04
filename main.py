b93289d8000057fe90702986ffaeab581c6c2d1a
CorderoMee <gjaimes499@unab.edu.co>
2025-04-04 08:22:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import flet as ft
+
+def main(page: ft.Page):
+    # Configuración de la página
+    page.title = "App Hola Mundo"
+    page.vertical_alignment = "center"
+    page.horizontal_alignment = "center"
+    
+    # Texto grande centrado
+    titulo = ft.Text("Hola Mundo", size=40, weight="bold")
+    
+    # Botón
+    btn = ft.ElevatedButton("Saludar", width=200)
+    
+    # Función que se ejecuta al presionar el botón
+    def saludar(e):
+        page.add(ft.Text("olaaaaaa", size=20, color="blue"))
+    
+    btn.on_click = saludar
+    
+    # Añadir elementos a la página
+    page.add(titulo, btn)
+
+# Ejecutar la app (modo escritorio por defecto)
+ft.app(target=main)
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+flet run main.py --web --host 0.0.0.0 --port $PORT
